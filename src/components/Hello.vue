9d969848bc98e6ab027b6d8c30dc4c6cfe31fdc6
Salman Sajid <sajidsalman75@gmail.com>
2023-02-12 16:30:07 +0500

    chore: setup eslint

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 const props = defineProps<{
-	name: string
+  name: string
 }>()
 
-const uppercase = computed(() => props.name.toLocaleUpperCase());
+const uppercase = computed(() => props.name.toLocaleUpperCase())
 </script>
 
 <template>
-	<div text="red 3xl" font-sans>Hello, {{ uppercase }}</div>
-  <div i-carbon-bee text-5xl text-yellow></div>
+  <div text="red 3xl" font-sans>
+    Hello, {{ uppercase }}
+  </div>
+  <div i-carbon-bee text-5xl text-yellow />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import UnoCSS from 'unocss/vite';
-import { presetAttributify, presetUno, presetIcons } from 'unocss';
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(),Components(), AutoImport({
-    imports: 'vue'
+  plugins: [Vue(), Components(), AutoImport({
+    imports: 'vue',
   }), UnoCSS({
     presets: [
       presetUno(),
       presetAttributify(),
-      presetIcons()
-    ]
+      presetIcons(),
+    ],
   })],
 })
