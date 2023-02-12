3bcece1cd177dfabbad1654392767dad581a969c
Salman Sajid <sajidsalman75@gmail.com>
2023-02-12 16:10:48 +0500

    feat: setup unocss

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import AutoImport from 'unplugin-auto-import/vite';
+import UnoCSS from 'unocss/vite';
+import { presetAttributify, presetUno, presetIcons } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [Vue(),Components(), AutoImport({
     imports: 'vue'
+  }), UnoCSS({
+    presets: [
+      presetUno(),
+      presetAttributify(),
+      presetIcons()
+    ]
   })],
 })
