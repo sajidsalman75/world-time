10f49e4ecfd95237ff8a0fc7217f08e3d2dcde4c
Salman Sajid <sajidsalman75@gmail.com>
2023-02-12 15:50:25 +0500

    feat: setup auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(),Components()],
+  plugins: [Vue(),Components(), AutoImport({
+    imports: 'vue'
+  })],
 })
